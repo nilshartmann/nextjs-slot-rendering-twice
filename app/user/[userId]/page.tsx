34d7a0625a16b96ea496f65839e4e9570f15a637
Nils Hartmann <nils@nilshartmann.net>
2023-09-26 14:24:36 +0200

    static params

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -27,3 +27,8 @@ export default function UserDetailPage({ params }: UserDetailPageProps) {
     </div>
   );
 }
+
+// same behaviour with generateStaticParams:
+// export async function generateStaticParams() {
+//   return [{ userId: "1" }, { userId: "2" }, { userId: "3" }, { userId: "4" }];
+// }
